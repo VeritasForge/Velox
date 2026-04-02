a1a304b844662afb8b10aea7e6247f41a8df705c
VeritasForge <greenfrog@outlook.kr>
2026-04-02 16:53:18 +0900

    feat(session): disable netrw and add stdin detection

--- init.lua
+++ init.lua
@@ -1,6 +1,17 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- netrw 비활성화 (Neo-tree가 대체)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- stdin 파이프 감지 (세션 자동 복원 방지용)
+vim.api.nvim_create_autocmd("StdinReadPre", {
+  callback = function()
+    vim.g.started_with_stdin = true
+  end,
+})
+
 require("config.options")
 require("config.keymaps")
 require("config.lazy")
